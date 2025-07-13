5884fa3665af73f4484cb3f8202f876c97d16625
hadi karami <persianphpprogrammer@gmail.com>
2025-07-13 06:42:49 -0700

    update 3

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\Api\ItemController;
 
 Route::prefix('items')->group(function (){
-   Route::get('/' ,[\App\Http\Controllers\Api\ItemController::class , 'index'] );
+   Route::get('/' ,[ItemController::class , 'index'] );
    Route::post('create' , [ItemController::class , 'create']);
 });
